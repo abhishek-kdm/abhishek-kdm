c05d0700068b1509fa66ec05920a435e119ef61d
Abhishek S. Kadam <abhishekk19@gmail.com>
2020-01-26 10:26:22 +0530

    adding components to main.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+import Header from './components/Header/header.component';
+import Home from './components/Home/home.component';
+import Footer from './components/Footer/footer.component';
+
+import { AppContext } from './context';
+
+
 const App: React.FC = () => {
+  const [title, setTitle] = useState<string>('Home | Abhishek Kadam');
+
+  // setting title on page load or title change.
+  useEffect(() => { document.title = title; }, [title]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext.Provider value={{ setTitle }}>
+      <div id={'wrapper'}>
+        <Header />
+        <Home />
+        <Footer />
+      </div>
+    </AppContext.Provider>
   );
 }
 
