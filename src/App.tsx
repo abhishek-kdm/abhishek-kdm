a2b97eb47a586e4287e942219fa6b793ee2a263c
Abhishek S. Kadam <abhishekk19@gmail.com>
2020-01-28 14:26:50 +0530

    Bringing stuff together, warp loading etc.

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,31 @@ import Footer from './components/Footer/footer.component';
 import { AppContext } from './context';
 import ThemeProvider from './components/ThemeProvider/themeProvider.component';
 import FloatingButton from './components/__pure__/FloatingButton/floatingButton.component';
+import Warpgate from './components/__pure__/Warpgate/warpgate.component';
 
 
 const App: React.FC = () => {
   const [title, setTitle] = useState<string>('Home | Abhishek Kadam');
+  const [warped_v, setWarped_v] = useState<boolean>(false);
+  const [warped_h, setWarped_h] = useState<boolean>(false);
 
   // setting title on page load or title change.
   useEffect(() => { document.title = title; }, [title]);
 
+  useEffect(() => {
+    setTimeout(() => { setWarped_h(true); }, 1000);
+  }, []);
+  useEffect(() => {
+    if (warped_h) {
+      setTimeout(() => { setWarped_v(true); }, 300);
+    }
+  }, [warped_h]);
+
   return (
     <AppContext.Provider value={{ setTitle }}>
       <div id={'wrapper'}>
+        <Warpgate open={warped_v} orientation={'vertical'} />
+        <Warpgate open={warped_h} orientation={'horizontal'} />
         <ThemeProvider />
         <Header />
         <Home />
