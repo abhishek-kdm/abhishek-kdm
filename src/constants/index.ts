da97750a319e8e8f60392591ac0cf6db93740eac
Abhishek S. Kadam <abhishekk19@gmail.com>
2020-01-28 14:19:39 +0530

    Navbar constants removed (added to the source file itself).

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,9 +2,3 @@ export const THEME: { [name: string]: Theme } = {
   dark: 'dark',
   light: 'light'
 };
-
-
-export const NAVBAR_ITEMS: NavbarItem[] = [
-  { label: 'aboutme', link: 'https://abhishek-kdm.github.io/aboutme/' },
-  { label: 'projects', link: 'https://github.com/abhishek-kdm/' },
-];
