89bd5ebc68afd0d9b78e3449967ed416449d3263
Abhishek S. Kadam <abhishekk19@gmail.com>
2020-01-29 22:25:26 +0530

    Social component.

--- src/components/Social/social.component.tsx
+++ src/components/Social/social.component.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import './social.style.css';
 
+import { SOCIAL_LINKS } from '../../configs';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 interface SocialProps { }
  
 const Social: React.FC<SocialProps> = () => {
+
   return (<>
-    <div className='social'>
-      {'This is where all my social network links would show up.'}
+    <div id='social'>
+      <div className='link-container'>
+        {SOCIAL_LINKS().map(({ title, href, iconProps }, i) => (
+          <a
+            key={i}
+            className={'link'}
+            title={title}
+            href={href}
+            target='_blank' rel='noopener noreferrer'
+          >
+            <FontAwesomeIcon {...iconProps} size={'3x'} />
+          </a>
+        ))}
+      </div>
     </div>
   </>);
 }
