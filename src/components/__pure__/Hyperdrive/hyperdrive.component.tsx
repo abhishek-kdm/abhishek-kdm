aaebda18be1f3a94609e936c29c941cdc66fda03
Abhishek S. Kadam <abhishekk19@gmail.com>
2020-02-04 12:30:05 +0530

    float button vintage mode, float button in hyperdrive for info.

--- src/components/__pure__/Hyperdrive/hyperdrive.component.tsx
+++ src/components/__pure__/Hyperdrive/hyperdrive.component.tsx
@@ -1,11 +1,15 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 
 import Hyperdrive from './hyperdrive';
+import FloatButton from '../FloatingButton/floatingButton.component';
+import { AppContext } from '../../../context';
+
 
 interface HyperdriveProps
   extends React.HTMLAttributes<HTMLCanvasElement> { }
- 
+
 const HyperdriveComponent: React.FC<HyperdriveProps> = (props) => {
+  const { setModalShow } = useContext(AppContext);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
@@ -17,7 +21,15 @@ const HyperdriveComponent: React.FC<HyperdriveProps> = (props) => {
     }
   }, [canvasRef]);
 
-  return <canvas {...props} id="canvas" ref={canvasRef} />;
+  return (<>
+    <FloatButton vintage
+      style={{ width: '30px', height: '30px', zIndex: 10001 }}
+      onClick={() => { setModalShow(true); }}
+    >
+      {'?'}
+    </FloatButton>
+    <canvas {...props} id="canvas" ref={canvasRef} />
+  </>);
 }
  
 export default HyperdriveComponent;
