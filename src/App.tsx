8c9da8179c8c78573b42a98ccc1d5012d7f742e4
Abhishek S. Kadam <abhishekk19@gmail.com>
2020-02-04 12:30:16 +0530

    github integration.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,26 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 import { AppContext } from './context';
-import { WARPSTATE } from './configs';
+import { WARPSTATE, GITHUB, THEME } from './configs';
 
 import SpaceExplorer from './components/SpaceExplorer/spaceExplorer.component';
 
 import FloatButton from './components/__pure__/FloatingButton/floatingButton.component';
+import { fetchJson } from './utils';
+import Modal from './components/__pure__/Modal/modal.component';
+import InfoBox from './components/__pure__/InfoBox/infoBox.component';
 
 
 const App: React.FC = () => {
   const [title, setTitle] = useState<string>('Home | Abhishek Kadam');
   const [warpState, setWarpState] = useState<WarpState>(WARPSTATE.warpin);
+  const [theme, setTheme] = useState<Theme>(THEME.dark);
+
+  // github links and info etc.
+  const [user, setUser] = useState<any>(null);
+  const [repos, setRepos] = useState<any[]>([]);
+
+  const [modalShow, setModalShow] = useState<boolean>(false);
 
   useEffect(() => {
     const escapeHandler = (e: KeyboardEvent) => {
@@ -22,26 +32,67 @@ const App: React.FC = () => {
       }
     }
     document.addEventListener('keydown', escapeHandler);
-
-    return () => {
-      document.removeEventListener('keydown', escapeHandler);
-    }
+    return () => { document.removeEventListener('keydown', escapeHandler); }
   }, [warpState]);
-
 
   // setting title on page load or title change.
   useEffect(() => { document.title = title; }, [title]);
 
+  useEffect(() => {
+    fetchJson(GITHUB.user_url)
+      .then((_user) => { setUser(_user); });
+  }, []);
+
+  useEffect(() => {
+    if (user != null) {
+      fetchJson(user.repos_url)
+        .then((repos) => { setRepos(repos); });
+    }
+  }, [user]);
+
 
   return (
-    <AppContext.Provider value={{ setTitle }}>
+    <AppContext.Provider value={{
+      theme,
+      setTheme,
+      setModalShow,
+      user,
+      repos,
+      setTitle
+    }}>
       <div id={'wrapper'}>
         <SpaceExplorer warpState={warpState} /> 
 
-        <FloatButton onClick={() => { setWarpState(WARPSTATE.explore) }}>
-          {'Explore'}
-          {'Space'}
+        <FloatButton
+          style={{ width: '75px', height: '75px' }}
+          onClick={() => { setWarpState(WARPSTATE.explore) }}
+        >
+          {'eject'}
         </FloatButton>
+
+        <Modal
+          show={modalShow}
+          closeFunc={() => { setModalShow(false); }}
+        >
+          <InfoBox animate={true} style={{ textAlign: 'center' }}>
+            <p>
+            <span>{'Navigate with '}</span>
+            <span style={{ color: 'white' }}>{'`arrows`.'}</span><br />
+            </p>
+
+            <p>
+            <span>{'To go faster in '}</span>
+            <span style={{ color: 'white' }}>{'`space`'}</span>
+            <span>{', push it.'}</span><br />
+            </p>
+
+            <p>
+            <span>{'to '}</span>
+            <span style={{ color: 'white' }}>{'`escape`'}</span>
+            <span>{', just do so.'}</span><br />
+            </p>
+          </InfoBox>
+        </Modal>
       </div>
     </AppContext.Provider>
   );
--- src/components/Projects/projects.component.tsx
+++ src/components/Projects/projects.component.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './projects.style.css';
+
+import { AppContext } from '../../context';
+
+import Title from '../__pure__/Title/title.component';
 
 
 interface ProjectsProps { }
  
 const Projects: React.FC<ProjectsProps> = () => {
+  const { user, repos } = useContext(AppContext);
+
   return (<>
+    <Title>
+      <a href={user.html_url} target='_blank' rel="noopener noreferrer">
+        {user.html_url}
+      </a>
+    </Title>
     <div id='projects'>
 
-      {/* styleing global for `.title` */}
-      <div className='title'></div>
 
       <ul className={'list'}>
-        <li><a href=''>{'kbasd'}</a></li>
-        <li><a href=''>{'jbasdf'}</a></li>
+        {repos.map((repo: any) => (
+          <li key={repo.id}>
+            <a
+              href={repo.html_url}
+              target='_blank'
+              rel="noopener noreferrer"
+              className={repo.description ? 'tooltip-parent' : ''}
+            >
+              {repo.description &&
+                <span className={'tooltip-text'}>{repo.description}</span>}
+              {repo.name}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   </>);
