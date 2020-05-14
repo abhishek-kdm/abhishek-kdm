d0282d6e0d2f56e834d5f588972efaaa9abaa6c5
Abhishek <abhishekk19@gmail.com>
2020-05-14 08:06:44 +0530

    header animations styling.

--- src/components/Header/header.style.ts
+++ src/components/Header/header.style.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { prepareLaunch, launchReady } from './header.animations';
 
 
 const StyledHeader = styled.header`
@@ -34,36 +35,12 @@ export const HeaderTitle = styled.h1`
 export const RocketImg = styled.img`
   height: 45px;
   cursor: pointer;
-  animation: prepare-launch 3.5s ease-in infinite;
+  animation: ${prepareLaunch} 3.5s ease-in infinite;
 
   &:hover {
-    animation: launch-ready .35s ease-in infinite;
+    animation: ${launchReady} .35s ease-in infinite;
   }
 
-  @keyframes prepare-launch {
-    10% { transform: translateY(-4px); }
-    25% { transform: translateY(-2.5px); }
-    40% { transform: translateY(-4px); }
-    55% { transform: translateY(2.5px); }
-    70% { transform: translateY(0px); }
-    85% { transform: translateY(-4px); }
-    95% { transform: translateY(-1.75px); }
-    100% { transform: translateY(0); }
-  }
-
-  @keyframes launch-ready {
-    0% { transform: translate(1px, 1px) rotate(0deg); }
-    10% { transform: translate(-1px, -2px) rotate(-1deg); }
-    20% { transform: translate(-3px, 0px) rotate(1deg); }
-    30% { transform: translate(3px, 2px) rotate(0deg); }
-    40% { transform: translate(1px, -1px) rotate(1deg); }
-    50% { transform: translate(-1px, 2px) rotate(-1deg); }
-    60% { transform: translate(-3px, 1px) rotate(0deg); }
-    70% { transform: translate(3px, 1px) rotate(-1deg); }
-    80% { transform: translate(-1px, -1px) rotate(1deg); }
-    90% { transform: translate(1px, 2px) rotate(0deg); }
-    100% { transform: translate(1px, -2px) rotate(-1deg); }
-  }
 `;
 
 export default StyledHeader;
