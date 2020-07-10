7e60d6dd4f6acf58ea328012a4ba77024a5b1702
Abhishek Kadam <abhishekk19@gmail.com>
2020-07-10 12:56:27 +0530

    - InfoBox component, animation updated. - 404 page removed. - build workflow updated, empty space line removed.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: `Simply computers, video games, food etc.`,
     author: `@lycuid`,
   },
+  assetPrefix: `https://${process.env.BUCKETNAME}.s3.amazonaws.com/`,
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
--- src/components/__pure__/InfoBox/infoBox.animations.ts
+++ src/components/__pure__/InfoBox/infoBox.animations.ts
@@ -1,7 +1,7 @@
 import { keyframes } from 'styled-components';
 
 export const vintageDisplay = keyframes`
-  0% { transform: rotatey(90deg) scaleY(.01); }
-  75% { transform: rotatey(0deg) scaleY(.01); }
+  0% { transform: scaleX(0.1) scaleY(.01); }
+  75% { transform: scaleX(1) scaleY(.01); }
   100% { transform: scaleY(1); }
 `;
