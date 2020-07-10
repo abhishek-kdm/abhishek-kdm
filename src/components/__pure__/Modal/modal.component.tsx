14521482bc5eca2c98e04471d2283075f3326443
Abhishek Kadam <abhishekk19@gmail.com>
2020-07-10 12:54:03 +0530

    Modal component, style css -> styled components, styling forwarded to component, cleanup, refactoring etc.

--- src/components/__pure__/Modal/modal.component.tsx
+++ src/components/__pure__/Modal/modal.component.tsx
@@ -1,26 +1,24 @@
-import React, { useCallback, useMemo, useEffect } from 'react';
-import './modal.style.css';
+import React, { useCallback, useEffect } from 'react';
+import StyledModal, { ModalContent } from './modal.style';
 
 
-interface ModalProps {
+interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
   show?: boolean
   opacity?: number,
+  offset?: string,
   closeFunc: () => void
 }
- 
+
 const Modal: React.FC<ModalProps> = ({
   show = false,
   opacity = .6,
+  offset = '100px',
   closeFunc,
-  children
+  ...props
 }) => {
   const handleClose = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
     if (e.target === e.currentTarget) { closeFunc(); }
   }, [closeFunc]);
-
-  const style = useMemo(() => ({
-    backgroundColor: `rgba(0, 0, 0, ${opacity})`,
-  }), [opacity]);
 
   useEffect(() => {
     const keyDownHandler = (e: KeyboardEvent) => {
@@ -35,14 +33,14 @@ const Modal: React.FC<ModalProps> = ({
 
   if (show) {
     return (
-      <div className='modal' style={style} onClick={handleClose}>
-        <div className='modal-content'>{children}</div>
-      </div>
+      <StyledModal offset={offset} opacity={opacity} onClick={handleClose}>
+        <ModalContent {...props} />
+      </StyledModal>
     );
   }
 
   return null;
 }
- 
+
 export default Modal;
 
